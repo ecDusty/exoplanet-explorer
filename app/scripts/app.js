665fc1563e2fa941b0a524958d027f99e76dca88
Erin <ecmgit@outlook.com>
2018-09-27 12:28:22 +0800

    ADD: Promise wrapped XML request

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -23,6 +23,7 @@ Instructions:
       response = JSON.parse(response).query;  // you'll be moving this line out of here in the next quiz!
     } catch (e) {
       // it's 'unknown', so leave it alone
+      response = "Error on JSON retreval: \n" + response;
     }
     home.innerHTML = '<h2 class="page-title">query: ' + response + '</h2>';
   }
@@ -36,22 +37,27 @@ Instructions:
     /*
     This code needs to get wrapped in a Promise!
      */
-    var req = new XMLHttpRequest();
-    req.open('GET', url);
-    req.onload = function() {
-      if (req.status === 200) {
-        // It worked!
-        // You'll want to resolve with the data from req.response
-      } else {
+    return new Promise(function(resolve,reject){
+      var req = new XMLHttpRequest();
+      req.open('GET', url);
+      req.onload = function() {
+        if (req.status === 200) {
+          // It worked!
+          // You'll want to resolve with the data from req.response
+          resolve(req.response)
+        } else {
+          // It failed :(
+          // Be nice and reject with req.statusText
+          this.onerror();
+        }
+      };
+      req.onerror = function() {
         // It failed :(
-        // Be nice and reject with req.statusText
-      }
-    };
-    req.onerror = function() {
-      // It failed :(
-      // Pass a 'Network Error' to reject
-    };
-    req.send();
+        // Pass a 'Network Error' to reject
+        reject(req.statusText);
+      };
+      req.send();
+    })
   }
 
   window.addEventListener('WebComponentsReady', function() {
@@ -61,6 +67,8 @@ Instructions:
     You'll need to add a .then and a .catch. Pass the response to addSearchHeader on resolve or
     pass 'unknown' to addSearchHeader if it rejects.
      */
-    // get('../data/earth-like-results.json')
+    get('../data/earth-like-results.json')
+    .then(addSearchHeader)
+    .catch(addSearchHeader);
   });
 })(document);
